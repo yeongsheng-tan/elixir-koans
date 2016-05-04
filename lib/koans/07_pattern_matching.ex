367f73e861938e6e827eda528d4d69d7c9e36b7f
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-05-04 12:07:03 +0800

    feat: solutions for 01 to 07

--- lib/koans/07_pattern_matching.ex
+++ lib/koans/07_pattern_matching.ex
@@ -2,56 +2,56 @@ defmodule PatternMatching do
   use Koans
 
   koan "One matches one" do
-    assert match?(1, ___)
+    assert match?(1, 1)
   end
 
   koan "A pattern can change" do
     a = 1
     a = 2
-    assert a == ___
+    assert a == 2
   end
 
   koan "A pattern can also be strict" do
     a = 1
     assert_raise MatchError, fn() ->
-      ^a = ___
+      ^a = 2
     end
   end
 
   koan "Patterns can be used to pull things apart" do
     [head | tail] = [1, 2, 3, 4]
 
-    assert head == ___
-    assert tail == ___
+    assert head == 1
+    assert tail == [2, 3, 4]
   end
 
   koan "And then put them back together" do
     head = 1
     tail = [2, 3, 4]
 
-    assert ___ == [head | tail]
+    assert [1, 2, 3, 4] == [head | tail]
   end
 
   koan "Some values can be ignored" do
     [_first, _second, third, _fourth] = [1, 2, 3, 4]
 
-    assert third == ___
+    assert third == 3
   end
 
   koan "Strings come apart just as easily" do
     "Shopping list: " <> items = "Shopping list: eggs, milk"
 
-    assert items == ___
+    assert items == "eggs, milk"
   end
 
   koan "Patterns show what you really care about" do
     %{make: make} = %{type: "car", year: 2016, make: "Honda", color: "black"}
 
-    assert make == ___
+    assert make == "Honda"
   end
 
   koan "The pattern can make assertions about what it expects" do
-    assert match?([1, _second, _third], ___)
+    assert match?([1, _second, _third], [1, 2, 3])
   end
 
   def make_noise(%{type: "cat"}), do: "Meow"
@@ -63,9 +63,9 @@ defmodule PatternMatching do
     dog = %{type: "dog"}
     snake = %{type: "snake"}
 
-    assert make_noise(cat) == ___
-    assert make_noise(dog) == ___
-    assert make_noise(snake) == ___
+    assert make_noise(cat) == "Meow"
+    assert make_noise(dog) == "Woof"
+    assert make_noise(snake) == "Eh?"
   end
 
   koan "And they will only run the code that matches the argument" do
@@ -75,9 +75,9 @@ defmodule PatternMatching do
       (_other)  -> "I need a name!"
     end
 
-    assert name.("mouse") == ___
-    assert name.("duck") == ___
-    assert name.("donkey") == ___
+    assert name.("mouse") == "Mickey"
+    assert name.("duck") == "Donald"
+    assert name.("donkey") == "I need a name!"
   end
 
   koan "Errors are shaped differently than successful results" do
@@ -88,7 +88,7 @@ defmodule PatternMatching do
       :error -> "not present"
     end
 
-    assert result == ___
+    assert result == "dog"
   end
 
   defmodule Animal do
@@ -97,6 +97,6 @@ defmodule PatternMatching do
 
   koan "You can pattern match into the fields of a struct" do
     %Animal{name: name} = %Animal{kind: "dog", name: "Max"}
-    assert name == ___
+    assert name == "Max"
   end
 end
